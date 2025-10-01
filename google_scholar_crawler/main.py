e5d78d0351f83a4a145edc9443a234155196ee2f
EchoPickle <1063841225@qq.com>
2025-10-01 23:04:44 +0800

    update

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,36 +1,47 @@
-from scholarly import scholarly
+from scholarly import scholarly, ProxyGenerator
 import jsonpickle
 import json
 from datetime import datetime
 import os
-print("DEBUG: script started!")
-scholar_id = os.environ.get('GOOGLE_SCHOLAR_ID')
-print("DEBUG: GOOGLE_SCHOLAR_ID =", scholar_id)
+from scholarly._proxy_generator import MaxTriesExceededException
 
-print("DEBUG: start search_author_id")
-author: dict = scholarly.search_author_id(scholar_id)
-print("DEBUG: finished search_author_id")
 
-print("DEBUG: start scholarly.fill")
-scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-print("DEBUG: finished scholarly.fill")
+try:
+    print("正在查找作者信息...")
+    # Setup proxy
+    pg = ProxyGenerator()
+    pg.FreeProxies()  # Use free rotating proxies
+    scholarly.use_proxy(pg)
+    print("DEBUG: script started!")
+    scholar_id = os.environ.get('GOOGLE_SCHOLAR_ID')
+    print("DEBUG: GOOGLE_SCHOLAR_ID =", scholar_id)
 
-name = author['name']
-author['updated'] = str(datetime.now())
-author['publications'] = {v['author_pub_id']:v for v in author['publications']}
-print(json.dumps(author, indent=2))
+    print("DEBUG: start search_author_id")
+    author: dict = scholarly.search_author_id(scholar_id)
+    print("DEBUG: finished search_author_id")
+except MaxTriesExceededException as e:
+    print(f"发生异常: {e}")
+else:
+  print("DEBUG: start scholarly.fill")
+  scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 
-print("DEBUG: start saving gs_data.json")
-os.makedirs('results', exist_ok=True)
-with open(f'results/gs_data.json', 'w') as outfile:
-    json.dump(author, outfile, ensure_ascii=False)
-print("DEBUG: saved gs_data.json")
+  name = author['name']
+  author['updated'] = str(datetime.now())
+  author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+  print(json.dumps(author, indent=2))
 
-shieldio_data = {
-  "schemaVersion": 1,
-  "label": "citations",
-  "message": f"{author['citedby']}",
-}
-with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
-    json.dump(shieldio_data, outfile, ensure_ascii=False)
-print("DEBUG: saved gs_data_shieldsio.json")
+  print("DEBUG: start saving gs_data.json")
+  os.makedirs('results', exist_ok=True)
+  with open(f'results/gs_data.json', 'w') as outfile:
+      json.dump(author, outfile, ensure_ascii=False)
+  print("DEBUG: saved gs_data.json")
+
+  shieldio_data = {
+    "schemaVersion": 1,
+    "label": "citations",
+    "message": f"{author['citedby']}",
+  }
+  with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+      json.dump(shieldio_data, outfile, ensure_ascii=False)
+  print("DEBUG: saved gs_data_shieldsio.json")
+  print("Done.")
